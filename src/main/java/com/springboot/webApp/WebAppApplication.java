591b7977fea139a0bb6bfdd731554d22518027e7
matiaskobold <matiaskobold@gmail.com>
2020-09-28 22:16:25 -0300

    Solved CORS warning.

--- src/main/java/com/springboot/webApp/WebAppApplication.java
+++ src/main/java/com/springboot/webApp/WebAppApplication.java
@@ -2,6 +2,9 @@ package com.springboot.webApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WebAppApplication {
@@ -9,5 +12,7 @@ public class WebAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebAppApplication.class, args);
 	}
+
+
 
 }
--- src/main/java/com/springboot/webApp/apiREST/UserControllerAPIREST.java
+++ src/main/java/com/springboot/webApp/apiREST/UserControllerAPIREST.java
@@ -6,13 +6,7 @@ import java.util.stream.Collectors;
 import com.springboot.webApp.repository.UserRepository;
 import com.springboot.webApp.model.User;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.hateoas.*;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
