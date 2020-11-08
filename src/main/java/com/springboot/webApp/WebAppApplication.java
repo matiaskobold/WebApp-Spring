5afb61435d24f2eea81ff148bddeae1538eb5ce5
matiaskobold <matiaskobold@gmail.com>
2020-11-08 20:11:19 -0300

    Added Steam API CS:GO News Support

--- src/main/java/com/springboot/webApp/WebAppApplication.java
+++ src/main/java/com/springboot/webApp/WebAppApplication.java
@@ -1,8 +1,11 @@
 package com.springboot.webApp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
--- src/main/java/com/springboot/webApp/controller/HomeController.java
+++ src/main/java/com/springboot/webApp/controller/HomeController.java
@@ -1,16 +1,38 @@
 package com.springboot.webApp.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springboot.webApp.model.User;
 import com.springboot.webApp.model.UserLogin;
 import com.springboot.webApp.repository.UserRepository;
 import com.springboot.webApp.service.UserLoginService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springboot.webApp.apiREST.ResourceNotFoundException;
+import com.springboot.webApp.model.Clan;
+import com.springboot.webApp.repository.ClanRepository;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import javax.xml.ws.Response;
 
 @Controller
 public class HomeController {
@@ -50,4 +72,44 @@ public class HomeController {
         userLoginService.saveUserLogin(userLogin);
         return "redirect:/";
     }
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+
+    @RequestMapping("/publicAPI")
+    public String publicAPI(Model model, RestTemplate restTemplate) throws JsonProcessingException {
+        int gameSteamID=730;
+        int numberOfNews=3;
+        int maxLengthOfNews=300;
+        String formatResponse="json";
+/*
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("appid",gameSteamID);
+        map.add("count", numberOfNews);
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map,headers);
+
+        ResponseEntity<String> responseStr = restTemplate.getForEntity(
+                "http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/",
+                String.class,
+                request);
+
+ */
+        ResponseEntity<String> responseStr=restTemplate.getForEntity("http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?"+
+                "appid="+gameSteamID+
+                "&count="+numberOfNews+
+                "&maxlength="+maxLengthOfNews+
+                "&format="+formatResponse,
+                String.class);
+
+
+
+        model.addAttribute("response", responseStr);
+        System.out.println(responseStr);
+        return "publicApi";
+    }
+
 }
